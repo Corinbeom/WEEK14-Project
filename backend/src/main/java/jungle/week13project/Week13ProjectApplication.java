16d0a0b15a82138d3f2aa0760d671d1b2917bb9d
hwaseongcityboy <eun4005@gmail.com>
2025-06-13 20:28:33 +0900

    FEAT: 초기 세팅 완료

---
+++ backend/src/main/java/jungle/week13project/Week13ProjectApplication.java
@@ -0,0 +1,13 @@
+package jungle.week13project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week13ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week13ProjectApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/jungle/week13project/Week13ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jungle.week13project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week13ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
